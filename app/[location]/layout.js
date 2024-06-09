2290bb7a637cc6fe6de1d81d49547f090ec72674
MD HASANUZZAMAN KHAN <khan.m.hasanuzzaman@gmail.com>
2024-06-09 15:21:00 +0600

    u

--- app/[location]/layout.js
+++ app/[location]/layout.js
@@ -1,4 +1,5 @@
 import { Inter } from "next/font/google";
+import Image from "next/image";
 import "../globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -11,11 +12,25 @@ export const metadata = {
 export default function RootLayout({ children, aqi, temperature, weather, wind }) {
     return (
         <div className="wrapper">
-            {children}
-            {aqi}
-            {temperature}
-            {weather}
-            {wind}
+            <div className="overlay"></div>
+            <Image
+                src="/background.png"
+                className="bg-img"
+                width={700}
+                height={1200}
+                alt="BG image"
+            />
+            <main className="!z-50 w-full">
+                <div className="container">
+                    <div className="grid grid-cols-12 gap-y-8 py-16 lg:gap-8 2xl:gap-20 2xl:py-20">
+                        {children}
+                        {weather}
+                        {aqi}
+                        {wind}
+                        {temperature}
+                    </div>
+                </div>
+            </main>
         </div>
     );
 }
