0d2f0dbcca28a871c2ffdfe4f63f8b9bd9f6f527
MD HASANUZZAMAN KHAN <khan.m.hasanuzzaman@gmail.com>
2024-06-10 21:46:19 +0600

    Location info, switcher and no loc component create

--- app/[location]/page.js
+++ app/[location]/page.js
@@ -1,10 +1,15 @@
+import LocationInfo from "@/components/LocationInfo";
+import NoLocationInfo from "@/components/NoLocationInfo";
+import { getResolvedLatLong } from "@/lib/location-info";
 
-const location = () => {
-    return (
-        <div>
-            Location page
-        </div>
-    );
-};
-
-export default location;
+export default async function Home({
+    params: { location },
+    searchParams: { latitude, longitude },
+}) {
+    const resolved = await getResolvedLatLong(location, latitude, longitude);
+    if (resolved?.lat && resolved?.lon) {
+        return <LocationInfo lat={resolved.lat} lon={resolved.lon} />;
+    } else {
+        return <NoLocationInfo />;
+    }
+}
